02a0a7482af5238e2ab2273fcba29c2502a857b9
supernebula <supernebula@126.com>
2017-06-27 13:18:33 +0800

    Demo upgrade to .netcore 2.0

--- src/Demo.Website/Controllers/ManageController.cs
+++ src/Demo.Website/Controllers/ManageController.cs
@@ -291,12 +291,13 @@ namespace Demo.Website.Controllers
                 return View("Error");
             }
             var userLogins = await _userManager.GetLoginsAsync(user);
-            var otherLogins = _signInManager.GetExternalAuthenticationSchemes().Where(auth => userLogins.All(ul => auth.AuthenticationScheme != ul.LoginProvider)).ToList();
+            var schemes = await _signInManager.GetExternalAuthenticationSchemesAsync();
+            var otherLogins = schemes.Where(auth => userLogins.All(ul => auth.Name != ul.LoginProvider)).ToList();
             ViewData["ShowRemoveButton"] = user.PasswordHash != null || userLogins.Count > 1;
             return View(new ManageLoginsViewModel
             {
                 CurrentLogins = userLogins,
-                OtherLogins = otherLogins
+                //OtherLogins = otherLogins //todo: .net core 1.1 升级 2.0后代码不兼容
             });
         }
 
--- src/DemoConsole/Program.cs
+++ src/DemoConsole/Program.cs
@@ -7,6 +7,7 @@ namespace DemoConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
