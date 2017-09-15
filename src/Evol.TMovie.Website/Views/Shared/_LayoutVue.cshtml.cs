ae3c07553381c998bee2641e944056c344594983
supernebula <supernebula@126.com>
2017-09-16 02:24:55 +0800

    first mobile page

---
+++ src/Evol.TMovie.Website/Views/Shared/_LayoutVue.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Evol.TMovie.Website.Views.Shared
+{
+    public class _LayoutVueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
