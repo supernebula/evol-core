c2fee49f1d8d7ea13c7ee033a9d59f3c2b30cc20
supernebula <supernebula@126.com>
2018-03-10 19:09:29 +0800

    Regex相关

--- src/Infrastructure/Evol.Util/RegexUtil.cs
+++ src/Infrastructure/Evol.Util/RegexUtil.cs
@@ -14,9 +14,21 @@ namespace Evol.Util
         /// <returns></returns>
         public static string KeepNumber(string str)
         {
-            var regex = new Regex(@"\d+");
-            var m = regex.Match(str);
-            return m.ToString();
+            var regex = new Regex(@"\D");
+            var str2 = regex.Replace(str, string.Empty);
+            return str2;
+        }
+
+        /// <summary>
+        /// 仅保留非数字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string KeepNonNumer(string str)
+        {
+            var regex = new Regex(@"\d");
+            var str2 = regex.Replace(str, string.Empty);
+            return str2;
         }
     }
 }
