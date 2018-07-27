7ca1c3cee6a399c3966a61fd9fc32ac24a939e93
supernebula <supernebula@126.com>
2018-07-27 15:28:58 +0800

    none

--- src/Infrastructure/Evol.Dapper/Repository/BasicDapperRepository.cs
+++ src/Infrastructure/Evol.Dapper/Repository/BasicDapperRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Dapper;
 using Evol.Common;
 
@@ -55,6 +56,15 @@ namespace Evol.Dapper.Repository
         public IEnumerable<T> Query(string sql, object param = null)
         {
             return DbConnection.Query<T>(sql, param);
+        }
+
+        public Tuple<IEnumerable<TResult1>, TResult2>  QueryMulti<TResult1, TResult2>(string sqlMulti, object param = null)
+        {
+            
+            var reader =  DbConnection.QueryMultiple(sqlMulti, param);
+            var result1 = reader.Read<TResult1>();
+            var result2 = reader.Read<TResult2>().FirstOrDefault();
+            return new Tuple<IEnumerable<TResult1>, TResult2>(result1, result2);
         }
 
 
---
+++ test/Evol.Util.Tests/TaskTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Evol.Util.Tests
+{
+    class TaskTest
+    {
+        public async Task TaskWhenAllTest()
+        {
+
+            var task1 = Task.Run(() => {
+                List<object> list = null;
+                //list  = db.select();
+                return list;
+            });
+
+            var task2 = Task.Run(() => {
+                List<object> list2 = null;
+                //list2  = db2.select();
+                return list2;
+            });
+
+
+            var listArr = await Task.WhenAll(task1, task2);
+
+            //listArr处理逻辑
+
+        }
+
+        public void TaskWhenAll2SyncTest()
+        {
+            var param1 = 0;
+            var task1 = Task.Run(() => {
+                List<object> list = null;
+                //list  = db.select(param1);
+                return list;
+            });
+
+            var task2 = Task.Run(() => {
+                List<object> list2 = null;
+                //list2  = db2.select();
+                return list2;
+            });
+
+
+
+            var t = Task.WhenAll(task1, task2);
+            t.Wait();
+            var listArr = t.Result;
+
+            //listArr处理逻辑
+
+        }
+    }
+}
