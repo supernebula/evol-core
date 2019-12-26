bc520b17370d08e685fb134896d0c8e2bf0ddb66
caozhongqiang <caozhongqiang@hzchangchong.com>
2019-12-26 19:16:37 +0800

    battery test

---
+++ test/Evol.InfluxDB.Tests/BatteryTest.cs
@@ -0,0 +1,137 @@
+﻿using AdysTech.InfluxDB.Client.Net;
+using Evol.InfluxDB.Tests.Model;
+using Evol.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Evol.InfluxDB.Tests
+{
+
+    public class BatteryTest
+    {
+        const string influxUrl = "https://ts-bp10ww8h1w50gud7k.influxdata.rds.aliyuncs.com:3242";
+        const string dbUName = "admin";
+        const string dbpwd = "Super2016";
+        const string dbName = "charge";
+        const string measurementName = "CarBattery";  //table
+
+        private ITestOutputHelper output;
+
+        public BatteryTest(ITestOutputHelper outputHelper)
+        {
+            output = outputHelper;
+        }
+
+        [Fact]
+        public void ConnectInfluxDBClientTest()
+        {
+            using (InfluxDBClient client = new InfluxDBClient(influxUrl, dbUName, dbpwd))
+            {
+                var temp = client;
+            }
+
+        }
+
+        [Fact]
+        public void TestGetInfluxDBNames()
+        {
+            using (InfluxDBClient client = new InfluxDBClient(influxUrl, dbUName, dbpwd))
+            {
+                TimeMonitor.Watch(nameof(TestGetInfluxDBNames), () =>
+                {
+                    var dbNames = client.GetInfluxDBNamesAsync().Result;
+                    var temp = dbNames;
+                }, output.WriteLine);
+            }
+        }
+
+
+
+        [Fact]
+        public void CreateInfluxDatabaseTest()
+        {
+            using (InfluxDBClient client = new InfluxDBClient(influxUrl, dbUName, dbpwd))
+            {
+                TimeMonitor.Watch(nameof(TestGetInfluxDBNames), () =>
+                {
+                    client.CreateDatabaseAsync("charge");
+                    var dbNames = client.GetInfluxDBNamesAsync().Result;
+                    var temp = dbNames;
+                }, output.WriteLine);
+            }
+        }
+
+        [Fact]
+        public void TestPostPointLoop()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(nameof(TestPostPointLoop) + "_" + i);
+                TestPostPoints();
+                Thread.Sleep(10000);
+
+
+            }
+        }
+
+        [Fact]
+        public void TestPostPoints()
+        {
+            try
+            {
+                var client = new InfluxDBClient(influxUrl, dbUName, dbpwd);
+                var time = DateTime.Now;
+                var today = DateTime.Now.ToShortDateString();
+                var now = DateTime.Now.ToShortTimeString();
+
+                var points = new List<IInfluxDatapoint>();
+
+                var item = new CarBattery();
+                item.DeviceNumber = "";
+                item.CustomerCode = "";
+                item.DeviceWays = "";
+                item.TransactionID = "";
+                item.VIN = "";
+                item.RVOL = DataGen.RandomDouble();
+                item.RCUR = DataGen.RandomDouble();
+                item.Ptemp = DataGen.RandomDouble();
+                item.Ntemp = DataGen.RandomDouble();
+                item.BATType = DataGen.RandomDouble();
+                item.BATPOW = DataGen.RandomDouble();
+                item.Capacity = DataGen.RandomDouble();
+                item.MaxCVol = DataGen.RandomDouble();
+                item.MaxCCur = DataGen.RandomDouble();
+                item.SigleMaxVol = DataGen.RandomDouble();
+                item.SigleMinVol = DataGen.RandomDouble();
+                item.StartSigleMaxVol = DataGen.RandomDouble();
+                item.StartSigleMinVol = DataGen.RandomDouble();
+                item.EndSigleMaxVol = DataGen.RandomDouble();
+                item.EndSigleMinVol = DataGen.RandomDouble();
+                item.MaxTemp = DataGen.RandomDouble();
+                item.StartVol = DataGen.RandomDouble();
+                item.CreateDate = DateTime.UtcNow;
+                item.MeasurementName = measurementName;
+                item.Precision = TimePrecision.Nanoseconds;
+                points.Add(item);
+
+               
+
+
+                var r = client.PostPointsAsync(dbName, points).Result;
+                Assert.True(r, "PostPointsAsync retunred false");
+            }
+            catch (Exception e)
+            {
+
+                //Assert.Fail($"Unexpected exception of type {e.GetType()} caught: {e.Message}");
+                return;
+            }
+        }
+
+
+    }
+}
---
+++ test/Evol.InfluxDB.Tests/Model/CarBattery.cs
@@ -0,0 +1,150 @@
+﻿using AdysTech.InfluxDB.Client.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evol.InfluxDB.Tests.Model
+{
+    public class CarBattery : InfluxDatapoint<double>
+    {
+        public string DeviceNumber
+        {
+            get  { return Tags.GetValueOrDefault(nameof(DeviceNumber)); }
+            set { this.Tags.Add(nameof(DeviceNumber), value); }
+        }
+
+        public string CustomerCode
+        {
+            get { return Tags.GetValueOrDefault(nameof(CustomerCode)); }
+            set { this.Tags.Add(nameof(CustomerCode), value); }
+        }
+
+        public string DeviceWays
+        {
+            get { return Tags.GetValueOrDefault(nameof(DeviceWays)); }
+            set { this.Tags.Add(nameof(DeviceWays), value); }
+        }
+
+        public string TransactionID
+        {
+            get { return Tags.GetValueOrDefault(nameof(TransactionID)); }
+            set { this.Tags.Add(nameof(TransactionID), value); }
+        }
+
+        public string VIN
+        {
+            get { return Tags.GetValueOrDefault(nameof(VIN)); }
+            set { this.Tags.Add(nameof(VIN), value); }
+        }
+
+        public double RVOL
+        {
+            get { return Fields.GetValueOrDefault(nameof(RVOL)); }
+            set { this.Fields.Add(nameof(RVOL), value); }
+        }
+
+        public double RCUR
+        {
+            get { return Fields.GetValueOrDefault(nameof(RCUR)); }
+            set { this.Fields.Add(nameof(RCUR), value); }
+        }
+
+        public double Ptemp
+        {
+            get { return Fields.GetValueOrDefault(nameof(Ptemp)); }
+            set { this.Fields.Add(nameof(Ptemp), value); }
+        }
+
+        public double Ntemp
+        {
+            get { return Fields.GetValueOrDefault(nameof(Ntemp)); }
+            set { this.Fields.Add(nameof(Ntemp), value); }
+        }
+
+        public double BATType
+        {
+            get { return Fields.GetValueOrDefault(nameof(BATType)); }
+            set { this.Fields.Add(nameof(BATType), value); }
+        }
+
+        public double BATPOW
+        {
+            get { return Fields.GetValueOrDefault(nameof(BATPOW)); }
+            set { this.Fields.Add(nameof(BATPOW), value); }
+        }
+
+        public double Capacity
+        {
+            get { return Fields.GetValueOrDefault(nameof(Capacity)); }
+            set { this.Fields.Add(nameof(Capacity), value); }
+        }
+
+        public double MaxCVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(MaxCVol)); }
+            set { this.Fields.Add(nameof(MaxCVol), value); }
+        }
+
+        public double MaxCCur
+        {
+            get { return Fields.GetValueOrDefault(nameof(MaxCCur)); }
+            set { this.Fields.Add(nameof(MaxCCur), value); }
+        }
+
+        public double SigleMaxVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(SigleMaxVol)); }
+            set { this.Fields.Add(nameof(SigleMaxVol), value); }
+        }
+
+        public double SigleMinVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(SigleMinVol)); }
+            set { this.Fields.Add(nameof(SigleMinVol), value); }
+        }
+
+        public double StartSigleMaxVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(StartSigleMaxVol)); }
+            set { this.Fields.Add(nameof(StartSigleMaxVol), value); }
+        }
+
+        public double StartSigleMinVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(StartSigleMinVol)); }
+            set { this.Fields.Add(nameof(StartSigleMinVol), value); }
+        }
+
+        public double EndSigleMaxVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(EndSigleMaxVol)); }
+            set { this.Fields.Add(nameof(EndSigleMaxVol), value); }
+        }
+
+        public double EndSigleMinVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(EndSigleMinVol)); }
+            set { this.Fields.Add(nameof(EndSigleMinVol), value); }
+        }
+
+        public double MaxTemp
+        {
+            get { return Fields.GetValueOrDefault(nameof(MaxTemp)); }
+            set { this.Fields.Add(nameof(MaxTemp), value); }
+        }
+
+        public double StartVol
+        {
+            get { return Fields.GetValueOrDefault(nameof(StartVol)); }
+            set { this.Fields.Add(nameof(StartVol), value); }
+        }
+
+
+
+        public DateTime CreateDate
+        {
+            get { return this.UtcTimestamp; }
+            set { this.UtcTimestamp = value; }
+        }
+    }
+}
